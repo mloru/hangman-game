2eb13cf043ebfb80d28e3aab23f62efabdbced90
Mirko Lorusso <mirko.lorusso@gmail.com>
2023-04-22 23:54:33 +0200

    Replace Jest with Cypress

--- __tests__/generateWord.spec.js
+++
@@ -1,8 +0,0 @@
-describe("Word generation", () => {
-    test("it should generate a word to guess", () => {
-        const outputType = "string";
-
-        expect(generateWordToBeGuessed()).toBeInstanceOf(outputType);
-        expect(generateWordToBeGuessed()).length.toBeGreaterThan(0);
-    });
-});
